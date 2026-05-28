f78509e7e7f2b10350d7a5784f4dff71dcbd90e9
PavanVibe <gpavanrahul@gmail.com>
2026-05-28 15:50:38 +0530

    debug: add CORS log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,7 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
   credentials: false,
 }));
+console.log('CORS set to allow all origins');
 app.use(morgan('dev'));
 app.use(express.json());
 
